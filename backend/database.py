266216337a5547f7239a9b906cea5845f093e58a
faizan <faizannoorudeen313@gmail.com>
2026-07-12 04:14:17 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# ശ്രദ്ധിക്കുക: 'your_password' എന്ന ഭാഗത്ത് pgAdmin തുറക്കാൻ ഉപയോഗിച്ച പാസ്‌വേഡ് കൊടുക്കുക.
+
 DATABASE_URL = "postgresql://postgres:faizan%40123@localhost:5432/resume_builder"
 
 engine = create_engine(DATABASE_URL)
